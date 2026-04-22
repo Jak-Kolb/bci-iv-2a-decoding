e4ba4be3777f72a879433421b39b3ca52d778470
Jak Kolb <fishinjak@gmail.com>
2026-04-22 13:43:15 -0400

    notebook one / data loading

--- src/mi_decoder/data.py
+++ src/mi_decoder/data.py
@@ -2,3 +2,11 @@
 from moabb.datasets import BNCI2014_001
 
 dataset = BNCI2014_001
+
+def load_subject(subject_id : int) -> dict:
+    # load single subjects data 
+    
+    data = BNCI2014_001().getdata(subjects=[subject_id])
+    # returns dict of shape {session_name: {run_name: mne.io.Raw}}
+    
+    return data[subject_id]
